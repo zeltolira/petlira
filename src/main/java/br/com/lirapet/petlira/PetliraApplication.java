5a93fee1b55c2d2bdad107d36e87989a99124e69
zelto@zeltolira.com.br <you@example.com>
2023-01-10 05:50:47 -0300

    inicia projeto

---
+++ src/main/java/br/com/lirapet/petlira/PetliraApplication.java
@@ -0,0 +1,13 @@
+package br.com.lirapet.petlira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetliraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetliraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lirapet/petlira/PetliraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lirapet.petlira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetliraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
