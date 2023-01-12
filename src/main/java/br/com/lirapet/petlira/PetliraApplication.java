9ff2661d91d7fa11ce9ac7e50f1f1d5312f16125
zelto@zeltolira.com.br <you@example.com>
2023-01-12 05:07:54 -0300

    implementa api de teste

--- src/main/java/br/com/lirapet/petlira/PetliraApplication.java
+++ src/main/java/br/com/lirapet/petlira/PetliraApplication.java
@@ -2,10 +2,20 @@ package br.com.lirapet.petlira;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class PetliraApplication {
 
+	@GetMapping
+	public String getHomeTeste() {
+		return "Cliente Pet - API Home";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PetliraApplication.class, args);
 	}
